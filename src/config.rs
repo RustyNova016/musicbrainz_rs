6e1d4a00cb7294f253ad5bd3bcc1dab2042ab74f
RustyNova <50844553+RustyNova016@users.noreply.github.com>
2025-04-22 14:45:36 +0000

    feat!: change api root to be only the domain

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 pub(crate) const DEFAULT_USER_AGENT: &str =
     concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
-pub(crate) const BASE_URL: &str = "http://musicbrainz.org/ws/2";
-pub(crate) const BASE_COVERART_URL: &str = "http://coverartarchive.org";
+pub(crate) const BASE_URL: &str = "musicbrainz.org";
+pub(crate) const BASE_COVERART_URL: &str = "https://coverartarchive.org";
 pub(crate) const FMT_JSON: &str = "?fmt=json";
 pub(crate) const PARAM_INC: &str = "&inc=";
 pub(crate) const PARAM_OFFSET: &str = "&offset=";
